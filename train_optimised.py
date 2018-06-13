e4bbf125ceed32427e9faf44a93d09b27f4197d1
Suruchi Chopra <suransh2008@gmail.com>
2018-06-13 22:31:50 +0530

    Added Tensorboard and Checkpoint Saving

--- train_optimised.py
+++ train_optimised.py
@@ -8,13 +8,15 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--n_hidden', type=int,
-                    help='Enter num of hidden LSTM cells', default = 256)
+                    help='Enter num of hidden LSTM cells', default = 512)
 parser.add_argument('--time_step', type=int,
                     help='Enter Time Step', default = 10)
 parser.add_argument('--lr', type=float,
                     help='Enter Learning Rate', default = 0.001)
 parser.add_argument('--nb_epoch', type=int,
                     help='Enter Number of Epochs', default = 100)
+parser.add_argument('--d_step', type=int,
+                    help='Enter Display Step', default = 1)
 
 
 args = parser.parse_args()
@@ -27,11 +29,13 @@ time_steps = args.time_step
 lr = args.lr
 num_epoch = args.nb_epoch
 num_total = num_notes + num_vel + num_time
+display_step = args.d_step
 
 print('[i] Number of Epochs:          ', num_epoch)
 print('[i] Learning Rate:          ', lr)
 print('[i] Number of hidden units:          ', n_hidden)
 print('[i] Time Step:          ', time_steps)
+print('[i] Display Step:          ', display_step)
 
 with tf.variable_scope('input'):
     note_x = tf.placeholder(dtype=tf.float32, shape=[None, time_steps, num_notes])
@@ -73,8 +77,14 @@ with tf.variable_scope('train'):
     opt_vel = tf.train.AdamOptimizer(learning_rate=lr).minimize(vel_cost)
     opt_time = tf.train.AdamOptimizer(learning_rate=lr).minimize(time_cost)
 
-init = tf.global_variables_initializer()
+with tf.variable_scope('logging'):
+    tf.summary.scalar('note_cost', note_cost)
+    tf.summary.scalar('velocity_cost', vel_cost)
+    tf.summary.scalar('time_cost', time_cost)
+    summary = tf.summary.merge_all()
 
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
 
 def Get_Next_X(note, vel, time, curr):
     n = np.array([note[curr: curr + time_steps]])
@@ -140,12 +150,15 @@ with tf.Session() as sess:
 
             tot_samples_train += curr
 
+            train_writer = tf.summary.FileWriter('./logs/training', sess.graph)
+            test_writer = tf.summary.FileWriter('./logs/testing', sess.graph)
+
             curr = 0
             while curr + time_steps < n_samples:
                 n_x, v_x, t_x = Get_Next_X(data['note_train'], data['vel_train'], data['time_train'], curr)
                 n_y, v_y, t_y = Get_Next_Y(data['note_train'], data['vel_train'], data['time_train'], curr)
 
-                n_loss, v_loss, t_loss = sess.run([note_cost, vel_cost, time_cost], feed_dict={
+                n_loss, v_loss, t_loss, train_summary = sess.run([note_cost, vel_cost, time_cost,summary], feed_dict={
                     note_x: n_x,
                     note_y: n_y,
                     vel_x: v_x,
@@ -169,7 +182,7 @@ with tf.Session() as sess:
                 n_x, v_x, t_x = Get_Next_X(data['note_test'], data['vel_test'], data['time_test'], curr)
                 n_y, v_y, t_y = Get_Next_Y(data['note_test'], data['vel_test'], data['time_test'], curr)
 
-                n_loss, v_loss, t_loss = sess.run([note_cost, vel_cost, time_cost], feed_dict={
+                n_loss, v_loss, t_loss, test_summary = sess.run([note_cost, vel_cost, time_cost, summary], feed_dict={
                     note_x: n_x,
                     note_y: n_y,
                     vel_x: v_x,
@@ -194,10 +207,17 @@ with tf.Session() as sess:
         test_vel_loss /= tot_samples_test
         test_time_loss /= tot_samples_test
 
+        if epoch % display_step == 0:
+            train_writer.add_summary(train_summary, epoch)
+            test_writer.add_summary(test_summary, epoch)
+            saver.save(sess, './logs/model_' + str(epoch) + '.ckpt')
+
         print ("Training Note Loss is {:,.6f} & Testing Note Loss is {:,.6f}".format(train_note_loss, test_note_loss))
         print ("Training Velocity Loss is {:,.6f} & Testing Velocity Loss is {:,.6f}".format(train_vel_loss, test_vel_loss))
         print ("Training Time Loss is {:,.6f} & Testing Time Loss is {:,.6f}".format(train_time_loss, test_time_loss))
 
-    print ("-------------------")
+        print ("-------------------")
 
     print("Training is Complete")
+    save_path = saver.save(sess, './logs/final.ckpt')
+    print ("Model saved: {}".format(save_path))
